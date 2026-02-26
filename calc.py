309483d44c47c5ab215e93ed14069931ba94d0e9
Семен Белый <belyjsema6@gmail.com>
2026-02-26 03:28:56 +0300

    Add add function

---
+++ calc.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    """Функция сложения"""
+    return a + b
+
+def subtract(a, b):
+    """Функция вычитания"""
+    return a - b
+
+if __name__ == "__main__":
+    print(f"5 + 3 = {add(5, 3)}")
+    print(f"10 - 4 = {subtract(10, 4)}")
